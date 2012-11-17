6f02dfcb7c184a0b13a5872bf56627824837424a
Simon Garcia <simon.garciar@gmail.com>
2012-11-17 17:31:16 -0430

    defining spec from example on p22

--- spec/chapters/ch3/catalog_spec.rb
+++ spec/chapters/ch3/catalog_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe Catalog do
   let!(:entry) { Entry.new("fish", "chips") }
+  let!(:other_entry) { Entry.new("fish", "chips") }
   let!(:catalog) { Catalog.new }
   let!(:add_entry) { catalog.add(entry) }
 
@@ -11,5 +12,13 @@ describe Catalog do
 
   it "indexes entries by name" do
     catalog.entry_for("fish").should be == entry
+  end
+
+  context "when adding an secondary entry" do
+    context "with the same name as another entry" do
+      it "raises an ArgumentError" do
+        expect {catalog.add(other_entry)}.to raise_error(ArgumentError)
+      end
+    end
   end
 end
