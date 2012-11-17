910bf4b406f0ad94ca99f5035175e239c6e46ebd
Simon Garcia <simon.garciar@gmail.com>
2012-11-17 17:33:07 -0430

    s/chips/peas on :other_entry

--- spec/chapters/ch3/catalog_spec.rb
+++ spec/chapters/ch3/catalog_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Catalog do
   let!(:entry) { Entry.new("fish", "chips") }
-  let!(:other_entry) { Entry.new("fish", "chips") }
+  let!(:other_entry) { Entry.new("fish", "peas") }
   let!(:catalog) { Catalog.new }
   let!(:add_entry) { catalog.add(entry) }
 
